2d06fa0793035558696c43d3764bb708537c6c2e
StefanoZaffiretti <10520809@polimi.it>
2020-03-22 22:17:12 +0100

    Aggiunto SetupMatch

---
+++ src/main/java/it/polimi/ingsw/SetupMatch.java
@@ -0,0 +1,18 @@
+package it.polimi.ingsw;
+
+import java.util.List;
+
+public class SetupMatch {
+
+    public List<God> godList;
+    public List<God> godPicked;
+    public List<Player> players;
+
+    public void PickGod() { };
+
+    public void CreateTable() { };
+
+    public void addGod(God) { };
+
+    public void addGodPicked(God) { };
+}
