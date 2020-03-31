22101da435167fdcbd26d9d16ae6506294e388f2
euge9 <eugenio.terzi@mail.polimi.it>
2020-03-31 14:30:30 +0200

    Aggiunto test di ForceMovementGods completo

--- src/test/java/it/polimi/ingsw/model/ForceMovementGodsTest.java
+++ src/test/java/it/polimi/ingsw/model/ForceMovementGodsTest.java
@@ -1,4 +1,25 @@
 package it.polimi.ingsw.model;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ForceMovementGodsTest {
+    @Test
+    public void GetTargetPosXTest()
+    {
+        ForceMovementGods forceMovementGods = new ForceMovementGods();
+        int x = 5;
+        forceMovementGods.setTargetPosX(x);
+        assertEquals("PosX is Wrong", forceMovementGods.getTargetPosX(), x);
+    }
+    @Test
+    public void GetTargetPosYTest()
+    {
+        ForceMovementGods forceMovementGods = new ForceMovementGods();
+        int y = 5;
+        forceMovementGods.setTargetPosX(y);
+        assertEquals("PosY is Wrong", forceMovementGods.getTargetPosX(), y);
+    }
+
 }
