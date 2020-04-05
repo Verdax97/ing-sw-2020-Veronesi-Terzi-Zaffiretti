0cde0c517c4675303a80aa6dc988d7b1d8d1b5dc
Verdax97 <Verdax97@users.noreply.github.com>
2020-04-05 17:46:26 +0200

    fixed end building phase

--- src/main/java/it/polimi/ingsw/model/Turn.java
+++ src/main/java/it/polimi/ingsw/model/Turn.java
@@ -49,16 +49,13 @@ public class Turn
         if ((x < 5 & x >= 0) & (y < 5 & y >= 0)) {
             if (this.selectedCell.isAdjacent(x, y)) {
                 if ((this.selectedCell.getBuilding() == board.getCell(x, y).getBuilding() + 1) || (this.selectedCell.getBuilding() >= board.getCell(x, y).getBuilding())) {
-                    if (!board.getCell(x, y).getDome() && board.getCell(x, y).getWorker() == null)
-                    {
+                    if (!board.getCell(x, y).getDome() && board.getCell(x, y).getWorker() == null) {
                         board.getCell(x, y).setWorker(this.selectedCell.getWorker());
                         this.selectedCell.getWorker().setLastMovement(board.getCell(x,y).getBuilding() - this.selectedCell.getBuilding());
                         this.selectedCell.setWorker(null);
                     } else throw new RuntimeException("Target cell is occupied");
                 } else throw new RuntimeException("Target cell is too high/low");
-            } else {
-                throw new RuntimeException("Target cell is too far");
-            }
+            } else throw new RuntimeException("Target cell is too far");
         } else throw new RuntimeException("Target cell out of board");
     }
 
@@ -69,18 +66,16 @@ public class Turn
                 if (board.getCell(x, y).getWorker() == null) {
                     if (!this.selectedCell.getDome()) {
                         int building = board.getCell(x, y).getBuilding();
-                        if (building < 3) {
+                        if (building < 3)
                             board.getCell(x, y).setBuilding(1);
-                        } else if (building == 3) {
+                        else if (building == 3)
                             board.getCell(x, y).setDome(true);
-                        }
                         board.getCell(x, y).setBuiltBy(this.selectedCell.getWorker().getPlayer());
+                        board.getCell(x, y).setBuiltTurn(turnNumber);
                     } else { throw new RuntimeException("Target cell has a Dome, you cannot build"); }
                 } else { throw new RuntimeException("Target cell has a worker on it, Baka");}
-            }
-            else { throw new RuntimeException("Target cell is too far!");}
-        }
-        else {throw new RuntimeException("Target cell is out of the board!");}
+            } else { throw new RuntimeException("Target cell is too far!");}
+        } else {throw new RuntimeException("Target cell is out of the board!");}
     }
 
     public void CheckLostOthers()
