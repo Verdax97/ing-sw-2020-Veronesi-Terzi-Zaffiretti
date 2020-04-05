11040ac593e57369c15ed17d45a518e972e96f2d
Verdax97 <Verdax97@users.noreply.github.com>
2020-04-05 13:45:43 +0200

    Created ServerView and ServerViewTest

--- src/main/java/it/polimi/ingsw/view/ServerView.java
+++ src/main/java/it/polimi/ingsw/view/ServerView.java
@@ -1,35 +1,92 @@
 package it.polimi.ingsw.view;
 
 import it.polimi.ingsw.model.Board;
+import it.polimi.ingsw.model.Match;
 import it.polimi.ingsw.model.SetupMatch;
 import it.polimi.ingsw.model.State;
 
+import java.io.PrintStream;
 import java.util.Observable;
 import java.util.Observer;
+import java.util.Scanner;
 
-public class ServerView extends Observable implements Runnable, Observer
+public class ServerView extends Observable implements Observer, Runnable
 {
-    private Board board;
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+
+    private Scanner scanner;
+    private PrintStream outputStream;
+    private boolean done = false;
+    //private Board board;
     private State state;
-    private SetupMatch setupMatch;
+    //private SetupMatch setupMatch;
 
-    public void PrintBoard()
+    public ServerView ()
     {
-
+        scanner = new Scanner(System.in);
+        outputStream = new PrintStream(System.out);
     }
 
-    public void ReciveMsg()
+    public void PrintBoard(Board board, Match match)
+    {
+        for(int i = 0; i < 5; i++){
+            for(int j = 0; j < 5; j++){
+                if (board.getCell(i, j).getDome())
+                {
+                    outputStream.print("D" + " ");
+                }
+
+                else
+                    if (board.getCell(i, j).getWorker() != null)
+                    {
+                        if (match.getPlayers().get(0).getNickname().equals(board.getCell(i, j).getWorker().getPlayer().getNickname()))
+                        {
+                            System.out.print(ANSI_RED + board.getCell(i, j).getBuilding()+ANSI_RESET + " ");
+                        }
+                        else if (match.getPlayers().get(1).getNickname().equals(board.getCell(i, j).getWorker().getPlayer().getNickname()))
+                        {
+                            System.out.print(ANSI_BLUE + board.getCell(i, j).getBuilding() + ANSI_RESET + " ");
+                        }
+                        else if (match.getPlayers().get(2).getNickname().equals(board.getCell(i, j).getWorker().getPlayer().getNickname()))
+                        {
+                            System.out.print(ANSI_GREEN + board.getCell(i, j).getBuilding() + ANSI_RESET + " ");
+                        }
+                    }
+                    else
+                    {
+                        System.out.print(board.getCell(i, j).getBuilding() + " ");
+                    }
+            }
+            outputStream.println();
+        }
+        outputStream.println();
+    }
+
+    public void ReceiveMsg()
     {
 
     }
 
     @Override
-    public void run() {
+    public void run()
+    {
 
     }
 
     @Override
-    public void update(Observable o, Object arg) {
-
+    public void update(Observable o, Object arg)
+    {
+        if(!(o instanceof Match) || !(arg instanceof Board)){
+            throw new IllegalArgumentException();
+        }
+        PrintBoard((Board)arg, (Match)o);
     }
 }
--- src/test/java/it/polimi/ingsw/view/ServerViewTest.java
+++ src/test/java/it/polimi/ingsw/view/ServerViewTest.java
@@ -1,8 +1,6 @@
 package it.polimi.ingsw.view;
 
-import it.polimi.ingsw.model.Board;
-import it.polimi.ingsw.model.Match;
-import it.polimi.ingsw.model.SetupMatch;
+import it.polimi.ingsw.model.*;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -14,13 +12,31 @@ public class ServerViewTest
     @Test
     public void PrintBoardTest()
     {
-        ArrayList<String> players = new ArrayList<String>();
+        ArrayList<String> players = new ArrayList<>();
         players.add("pino");
         players.add("pippo");
+        players.add("Magnifico Rettore Ferruccio Resta");
         Match match = new Match(players);
         SetupMatch setupMatch = new SetupMatch();
-        Board board = new Board();
         ServerView serverView = new ServerView();
-        serverView.PrintBoard(board, match);
+        Board board = match.getBoard();
+        board.getCell(4,4).setDome(true);
+        board.getCell(3,3).setBuilding(3);
+        board.getCell(2,2).setBuilding(2);
+        board.getCell(1,1).setBuilding(1);
+        Worker testWorker = new Worker();
+        testWorker.setPlayer(new Player(players.get(0)));
+        Worker testWorker2 = new Worker();
+        testWorker2.setPlayer(new Player(players.get(1)));
+        Worker testWorker3 = new Worker();
+        testWorker3.setPlayer(new Player(players.get(2)));
+        board.getCell(0, 4).setWorker(testWorker);
+        board.getCell(1, 3).setWorker(testWorker2);
+        board.getCell(2, 2).setWorker(testWorker3);
+        board.getCell(3, 1).setWorker(testWorker2);
+        board.getCell(4, 0).setWorker(testWorker);
+        board.getCell(4, 4).setWorker(testWorker3);
+        board.getCell(0, 0).setWorker(testWorker3);
+        serverView.PrintBoard(match.getBoard(), match);
     }
 }
