7c753182c849208e826e50fabfe857631d0574ce
Verdax97 <Verdax97@users.noreply.github.com>
2020-04-05 19:01:46 +0200

    Corrected Artemis and created ArtemisTest

--- src/main/java/it/polimi/ingsw/model/gods/Artemis.java
+++ src/main/java/it/polimi/ingsw/model/gods/Artemis.java
@@ -6,6 +6,8 @@ import it.polimi.ingsw.model.MultipleActionGod;
 
 public class Artemis extends MultipleActionGod
 {
+    private int lastX;
+    private int lastY;
     public Artemis(){
             this.name = "Artemis";
             this.description = "Your move: Your worker may move one additional time, but not back to the space it started on.";
@@ -15,16 +17,23 @@ public class Artemis extends MultipleActionGod
     @Override
     public int Move(Board board, Cell selectedCell, int x, int y)
     {
+        if (use == 0)
+        {
+            lastX = selectedCell.getPos()[0];
+            lastY = selectedCell.getPos()[1];
+        }
         if ((x < 5 & x >= 0) & (y < 5 & y >= 0)) {
             if (selectedCell.isAdjacent(x, y)) {
-                if ((selectedCell.getBuilding() == board.getCell(x, y).getBuilding() + 1) || (selectedCell.getBuilding() >= board.getCell(x, y).getBuilding())) {
-                    if (!board.getCell(x, y).getDome() && board.getCell(x, y).getWorker() == null) {
-                        board.getCell(x, y).setWorker(selectedCell.getWorker());
-                        selectedCell.getWorker().setLastMovement(board.getCell(x,y).getBuilding() - selectedCell.getBuilding());
-                        selectedCell.setWorker(null);
-                        use++;
-                    } else throw new RuntimeException("Target cell is occupied");
-                } else throw new RuntimeException("Target cell is too high/low");
+                if (x != lastX || y != lastY){
+                    if ((selectedCell.getBuilding() == board.getCell(x, y).getBuilding() + 1) || (selectedCell.getBuilding() >= board.getCell(x, y).getBuilding())) {
+                        if (!board.getCell(x, y).getDome() && board.getCell(x, y).getWorker() == null) {
+                            board.getCell(x, y).setWorker(selectedCell.getWorker());
+                            selectedCell.getWorker().setLastMovement(board.getCell(x,y).getBuilding() - selectedCell.getBuilding());
+                            selectedCell.setWorker(null);
+                            use++;
+                        } else throw new RuntimeException("Target cell is occupied");
+                    } else throw new RuntimeException("Target cell is too high/low");
+                } else throw new RuntimeException("Target cell is same as previous");
             } else throw new RuntimeException("Target cell is too far");
         } else throw new RuntimeException("Target cell out of board");
         return CheckUse();
--- src/test/java/it/polimi/ingsw/model/godsTest/ArtemisTest.java
+++ src/test/java/it/polimi/ingsw/model/godsTest/ArtemisTest.java
@@ -1,4 +1,46 @@
 package it.polimi.ingsw.model.godsTest;
 
-public class ArtemisTest {
+import it.polimi.ingsw.model.*;
+import it.polimi.ingsw.model.gods.Apollo;
+import it.polimi.ingsw.model.gods.Artemis;
+import it.polimi.ingsw.view.ServerView;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+public class ArtemisTest
+{
+    @Test
+    public void MoveTest()
+    {
+        ArrayList<String> players = new ArrayList<>();
+        players.add("pino");
+        players.add("pippo");
+        players.add("Magnifico Rettore Ferruccio Resta");
+        Match match = new Match(players);
+        ServerView serverView = new ServerView();
+        Board board = match.getBoard();
+        Worker testWorker = new Worker();
+        testWorker.setPlayer(new Player(players.get(0)));
+        Worker testWorker2 = new Worker();
+        testWorker2.setPlayer(new Player(players.get(1)));
+        Worker testWorker3 = new Worker();
+        testWorker3.setPlayer(new Player(players.get(2)));
+        board.getCell(0, 4).setWorker(testWorker);
+        board.getCell(1, 3).setWorker(testWorker2);
+        board.getCell(2, 2).setWorker(testWorker3);
+        board.getCell(3, 1).setWorker(testWorker2);
+        board.getCell(4, 0).setWorker(testWorker);
+        board.getCell(0, 0).setWorker(testWorker3);
+        Artemis artemis = new Artemis();
+        serverView.PrintBoard(board, match);
+        Cell selectedCell = board.getCell(0, 0);
+        assertEquals("Return value is wrong", 2, artemis.Move(board, selectedCell, 1, 1));
+        serverView.PrintBoard(board, match);
+        selectedCell = board.getCell(1, 1);
+        assertEquals("Return value is wrong", 1, artemis.Move(board, selectedCell, 0, 1));
+        serverView.PrintBoard(board, match);
+    }
 }
