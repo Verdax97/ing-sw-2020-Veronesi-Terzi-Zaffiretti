f77601c9100b7e94fd112ffef43b1045567d746c
Verdax97 <Verdax97@users.noreply.github.com>
2020-04-06 13:07:03 +0200

    Created Minotaur

--- src/main/java/it/polimi/ingsw/model/gods/Minotaur.java
+++ src/main/java/it/polimi/ingsw/model/gods/Minotaur.java
@@ -1,8 +1,25 @@
 package it.polimi.ingsw.model.gods;
 
+import it.polimi.ingsw.model.Board;
+import it.polimi.ingsw.model.Cell;
 import it.polimi.ingsw.model.ForceMovementGods;
 
 public class Minotaur extends ForceMovementGods
 {
-    
+    public Minotaur()
+    {
+        this.name = "Minotaur";
+        this.description = "Your Move: Your Worker may move into an opponent Worker’s space, if their Worker can be forced one space straight backwards to an unoccupied space at any level.";
+    }
+
+    @Override
+    public int Move(Board board, Cell selectedCell, int x, int y)
+    {
+        int dx = x - selectedCell.getPos()[0];
+        int dy = y - selectedCell.getPos()[1];
+        targetPosX = x + dx;
+        targetPosY = y + dy;
+        ForceMove(board, selectedCell, x, y);
+        return 1;
+    }
 }
