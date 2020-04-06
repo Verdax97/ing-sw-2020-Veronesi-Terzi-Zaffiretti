112d46201ba6c7f689e7bdb71fc3966b0b36ab42
StefanoZaffiretti <10520809@polimi.it>
2020-04-06 18:16:40 +0200

    Hephaestus + test

--- src/main/java/it/polimi/ingsw/model/gods/Hephaestus.java
+++ src/main/java/it/polimi/ingsw/model/gods/Hephaestus.java
@@ -1,8 +1,44 @@
 package it.polimi.ingsw.model.gods;
 
-import it.polimi.ingsw.model.God;
+import it.polimi.ingsw.model.Board;
+import it.polimi.ingsw.model.Cell;
+import it.polimi.ingsw.model.MultipleActionGod;
 
-public class Hephaestus extends God
-{
-    
+public class Hephaestus extends MultipleActionGod {
+
+    private int lastX;
+    private int lastY;
+
+    public Hephaestus(){
+        this.name = "Hephaestus";
+        this.description = "Your Build: your worker may build one additional block (not dome) on top of your first block";
+        this.useLimit = 2;
+    }
+
+    @Override
+    public int Building(Board board, Cell selectedCell, int x, int y, int typeBuild, int turnNumber){
+        if (use == 0)
+        {
+            lastX = x;
+            lastY = y;
+        }
+        if ((x < 5 & x >= 0) & (y < 5 & y >= 0)){
+            if (selectedCell.isAdjacent(x, y)) {
+                if (x == lastX || y == lastY) {
+                    if (board.getCell(x, y).getWorker() == null) {
+                        if (!selectedCell.getDome()) {
+                            int building = board.getCell(x, y).getBuilding();
+                            if (building < 3){
+                                board.getCell(x, y).setBuilding(1);
+                                board.getCell(x, y).setBuiltBy(selectedCell.getWorker().getPlayer());
+                                board.getCell(x, y).setBuiltTurn(turnNumber);
+                                use++;
+                            } else {throw new RuntimeException("You may build one additional block but not a dome");}
+                        } else { throw new RuntimeException("Target cell has a Dome, you cannot build"); }
+                    } else { throw new RuntimeException("Target cell has a worker on it"); }
+                } else {throw new RuntimeException("Target cell must be the same as previous");}
+            } else { throw new RuntimeException("Target cell is too far!");}
+        } else {throw new RuntimeException("Target cell is out of the board!");}
+        return CheckUse();
+    }
 }
--- src/test/java/it/polimi/ingsw/model/godsTest/HephaestusTest.java
+++ src/test/java/it/polimi/ingsw/model/godsTest/HephaestusTest.java
@@ -1,4 +1,44 @@
 package it.polimi.ingsw.model.godsTest;
 
+import it.polimi.ingsw.model.*;
+import it.polimi.ingsw.model.gods.Demeter;
+import it.polimi.ingsw.model.gods.Hephaestus;
+import it.polimi.ingsw.view.ServerView;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
 public class HephaestusTest {
+
+    @Test
+    public void BuildingTest(){
+        ArrayList<String> players = new ArrayList<>();
+        players.add("pino");
+        players.add("pippo");
+        players.add("Magnifico Rettore Ferruccio Resta");
+        Match match = new Match(players);
+        ServerView serverView = new ServerView();
+        Board board = match.getBoard();
+        Worker testWorker = new Worker();
+        testWorker.setPlayer(new Player(players.get(0)));
+        Worker testWorker2 = new Worker();
+        testWorker2.setPlayer(new Player(players.get(1)));
+        Worker testWorker3 = new Worker();
+        testWorker3.setPlayer(new Player(players.get(2)));
+        board.getCell(0, 4).setWorker(testWorker);
+        board.getCell(1, 3).setWorker(testWorker2);
+        board.getCell(2, 2).setWorker(testWorker3);
+        board.getCell(3, 1).setWorker(testWorker2);
+        board.getCell(4, 0).setWorker(testWorker);
+        board.getCell(0, 0).setWorker(testWorker3);
+        Hephaestus hephaestus = new Hephaestus();
+        serverView.PrintBoard(board, match);
+        Cell selectedCell = board.getCell(0, 0);
+        assertEquals("Return value is wrong", 2, hephaestus.Building(board, selectedCell, 1, 1, 0, 0));
+        serverView.PrintBoard(board, match);
+        assertEquals("Return value is wrong", 1, hephaestus.Building(board, selectedCell, 1, 1, 0, 0));
+        serverView.PrintBoard(board, match);
+    }
 }
