0bca67eba9a151ba9366168338110d6e68978066
StefanoZaffiretti <10520809@polimi.it>
2020-04-06 16:57:52 +0200

    Chrono test

--- src/test/java/it/polimi/ingsw/model/godsTest/ChronoTest.java
+++ src/test/java/it/polimi/ingsw/model/godsTest/ChronoTest.java
@@ -1,5 +1,36 @@
 package it.polimi.ingsw.model.godsTest;
 
-public class ChronoTest {
+import it.polimi.ingsw.model.Board;
+import it.polimi.ingsw.model.God;
+import it.polimi.ingsw.model.Player;
+import it.polimi.ingsw.model.Worker;
+import it.polimi.ingsw.model.gods.Chrono;
+import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+public class ChronoTest {
+    @Test
+    public void WinConditionTest(){
+        Board board = new Board();
+        Player player = new Player("Pino");
+        Worker worker = new Worker();
+        God chrono = new Chrono();
+        player.setGodPower(chrono);
+        worker.setPlayer(player);
+        board.getCell(3, 3).setWorker(worker);
+        board.getCell(0,0).setBuilding(3);
+        board.getCell(0,0).setDome(true);
+        board.getCell(0,1).setBuilding(3);
+        board.getCell(0,1).setDome(true);
+        board.getCell(0,2).setBuilding(3);
+        board.getCell(0,2).setDome(true);
+        board.getCell(0,3).setBuilding(3);
+        board.getCell(0,3).setDome(true);
+        assertNull("WinCondition negative Fail", player.getGodPower().WinCondition(board, player));
+        board.getCell(0,4).setBuilding(3);
+        board.getCell(0,4).setDome(true);
+        assertEquals("WinCondition Fail", player.getGodPower().WinCondition(board, player), player);
+    }
 }
