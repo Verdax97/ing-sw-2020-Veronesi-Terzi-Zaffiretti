df7f0af705f4298ee04dd2cd6dc3b72b47ced1c6
StefanoZaffiretti <10520809@polimi.it>
2020-04-06 17:29:28 +0200

    Demeter + test

--- src/main/java/it/polimi/ingsw/model/gods/Demeter.java
+++ src/main/java/it/polimi/ingsw/model/gods/Demeter.java
@@ -1,8 +1,45 @@
 package it.polimi.ingsw.model.gods;
 
-import it.polimi.ingsw.model.God;
+import it.polimi.ingsw.model.Board;
+import it.polimi.ingsw.model.Cell;
+import it.polimi.ingsw.model.MultipleActionGod;
 
-public class Demeter extends God
-{
-    
+public class Demeter extends MultipleActionGod {
+
+    private int lastX;
+    private int lastY;
+
+    public Demeter(){
+        this.name = "Demeter";
+        this.description = "Your Build: your worker may build one additional time, but not on the same space";
+        this.useLimit = 2;
+    }
+
+    @Override
+    public int Building(Board board, Cell selectedCell, int x, int y, int typeBuild, int turnNumber){
+        if (use == 0)
+        {
+            lastX = selectedCell.getPos()[0];
+            lastY = selectedCell.getPos()[1];
+        }
+        if ((x < 5 & x >= 0) & (y < 5 & y >= 0)){
+            if (selectedCell.isAdjacent(x, y)) {
+                if (x != lastX || y != lastY) {
+                    if (board.getCell(x, y).getWorker() == null) {
+                        if (!selectedCell.getDome()) {
+                            int building = board.getCell(x, y).getBuilding();
+                            if (building < 3)
+                                board.getCell(x, y).setBuilding(1);
+                            else if (building == 3)
+                                board.getCell(x, y).setDome(true);
+                            board.getCell(x, y).setBuiltBy(selectedCell.getWorker().getPlayer());
+                            board.getCell(x, y).setBuiltTurn(turnNumber);
+                            use++;
+                        } else { throw new RuntimeException("Target cell has a Dome, you cannot build"); }
+                    } else { throw new RuntimeException("Target cell has a worker on it"); }
+                } else {throw new RuntimeException("Target cell is same as previous");}
+            } else { throw new RuntimeException("Target cell is too far!");}
+        } else {throw new RuntimeException("Target cell is out of the board!");}
+        return CheckUse();
+    }
 }
--- src/test/java/it/polimi/ingsw/model/godsTest/DemeterTest.java
+++ src/test/java/it/polimi/ingsw/model/godsTest/DemeterTest.java
@@ -1,4 +1,45 @@
 package it.polimi.ingsw.model.godsTest;
 
-public class DemeterTest {
+import it.polimi.ingsw.model.*;
+import it.polimi.ingsw.model.gods.Artemis;
+import it.polimi.ingsw.model.gods.Demeter;
+import it.polimi.ingsw.view.ServerView;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+public class DemeterTest{
+
+    @Test
+    public void BuildingTest(){
+        ArrayList<String> players = new ArrayList<>();
+        players.add("pino");
+        players.add("pippo");
+        players.add("Magnifico Rettore Ferruccio Resta");
+        Match match = new Match(players);
+        ServerView serverView = new ServerView();
+        Board board = match.getBoard();
+        Worker testWorker = new Worker();
+        testWorker.setPlayer(new Player(players.get(0)));
+        Worker testWorker2 = new Worker();
+        testWorker2.setPlayer(new Player(players.get(1)));
+        Worker testWorker3 = new Worker();
+        testWorker3.setPlayer(new Player(players.get(2)));
+        board.getCell(0, 4).setWorker(testWorker);
+        board.getCell(1, 3).setWorker(testWorker2);
+        board.getCell(2, 2).setWorker(testWorker3);
+        board.getCell(3, 1).setWorker(testWorker2);
+        board.getCell(4, 0).setWorker(testWorker);
+        board.getCell(0, 0).setWorker(testWorker3);
+        Demeter demeter = new Demeter();
+        serverView.PrintBoard(board, match);
+        Cell selectedCell = board.getCell(0, 0);
+        assertEquals("Return value is wrong", 2, demeter.Building(board, selectedCell, 1, 1, 0, 0));
+        serverView.PrintBoard(board, match);
+        assertEquals("Return value is wrong", 1, demeter.Building(board, selectedCell, 0, 1, 0, 0));
+        serverView.PrintBoard(board, match);
+
+    }
 }
