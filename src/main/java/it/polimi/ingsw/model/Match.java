4b7f0e512594682d7d0a07768bbe7ad92917bc1c
Verdax97 <Verdax97@users.noreply.github.com>
2020-04-06 15:40:03 +0200

    Created Match.NextPlayer

--- src/main/java/it/polimi/ingsw/model/Match.java
+++ src/main/java/it/polimi/ingsw/model/Match.java
@@ -10,6 +10,7 @@ public class Match extends Observable
     private Turn turn;
     private SetupMatch setup;
     private Player winner;
+    private int nPlayer = 0;
 
     public Match(ArrayList<String> nicks)
     {
@@ -24,28 +25,24 @@ public class Match extends Observable
     {
 
     }
-    public Worker[] getActiveWorker(Player player)
+
+    public void NextTurn(Player player)
     {
-        Worker[] result = new Worker[2];
-        int k = 0;
-        for (int i = 0; i < 5; i++)
-        {
-            for (int j = 0; j < 5; j++)
-            {
-                if (board.getCell(i, j).getWorker().getPlayer().getNickname().compareTo(player.getNickname()) == 0)
-                {
-                    result[k] = board.getCell(i, j).getWorker();
-                    k++;
-                }
-                if (k == 2)
-                    return result;
-            }
-        }
-        return result;
+
     }
 
-    public void NextTurn(Player player) {
+    public Player NextPlayer()
+    {
+        if (nPlayer == setup.getPlayers().size())
+            nPlayer = 0;
+        else
+            nPlayer++;
+        return setup.getPlayers().get(0);
+    }
 
+    public void CheckWinCondition(Player player)
+    {
+        turn.CheckWinCondition(board, player);
     }
 
     public ArrayList<Player> getPlayers()
