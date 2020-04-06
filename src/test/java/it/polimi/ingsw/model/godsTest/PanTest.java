3559cd1dc316926e3d7ca21527232ce1addddfa8
euge9 <eugenio.terzi@mail.polimi.it>
2020-04-06 16:07:13 +0200

    Aggiunto Test di Pan

--- src/test/java/it/polimi/ingsw/model/godsTest/PanTest.java
+++ src/test/java/it/polimi/ingsw/model/godsTest/PanTest.java
@@ -1,4 +1,25 @@
 package it.polimi.ingsw.model.godsTest;
 
+import it.polimi.ingsw.model.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import it.polimi.ingsw.model.gods.Pan;
+import org.junit.Test;
+
 public class PanTest {
+    @Test
+    public void WinConditionTest(){
+        Board board = new Board();
+        Player player = new Player("Pino");
+        Worker worker = new Worker();
+        God pan = new Pan();
+        player.setGodPower(pan);
+        worker.setPlayer(player);
+        board.getCell(3, 3).setWorker(worker);
+        worker.setLastMovement(-2);
+        assertEquals("WinCondition negative Fail", player.getGodPower().WinCondition(board, player), player);
+        worker.setLastMovement(0);
+        assertNull("WinCondition negative Fail", player.getGodPower().WinCondition(board, player));
+    }
 }
