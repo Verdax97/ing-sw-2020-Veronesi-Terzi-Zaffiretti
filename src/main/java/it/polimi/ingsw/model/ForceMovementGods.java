7553b9a3601db1a7bfffabd7f6bcc2893a23452f
Verdax97 <Verdax97@users.noreply.github.com>
2020-04-18 16:28:35 +0200

    Fixed out of bound error when pushing worker outside the board

--- src/main/java/it/polimi/ingsw/model/ForceMovementGods.java
+++ src/main/java/it/polimi/ingsw/model/ForceMovementGods.java
@@ -6,6 +6,8 @@ public class ForceMovementGods extends God
     protected int targetPosY;
 
     protected int ForceMove(Board board, Cell selectedCell, int x, int y) {
+        if (targetPosX < 0 || targetPosX > 4 || targetPosY < 0 || targetPosY > 4)
+            return -6; //target space is out of board
         if ((x < 5 & x >= 0) & (y < 5 & y >= 0)) {
             if (selectedCell.isAdjacent(x, y)) {
                 if (selectedCell.IsNotHigh(board, x, y)) {
@@ -15,7 +17,7 @@ public class ForceMovementGods extends God
                             board.getCell(x, y).setWorker(selectedCell.getWorker());
                             selectedCell.getWorker().setLastMovement(board.getCell(x, y).getBuilding() - selectedCell.getBuilding());
                             selectedCell.setWorker(null);
-                        } else if (board.getCell(targetPosX, targetPosY).getWorker() == null || board.getCell(targetPosX, targetPosY).getWorker() == selectedCell.getWorker()) {
+                        } else if (board.getCell(targetPosX, targetPosY).getWorker() == null || !board.getCell(targetPosX, targetPosY).getDome() || board.getCell(targetPosX, targetPosY).getWorker() == selectedCell.getWorker()) {
                             board.getCell(x, y).setWorker(selectedCell.getWorker());
                             selectedCell.getWorker().setLastMovement(board.getCell(x, y).getBuilding() - selectedCell.getBuilding());
                             selectedCell.setWorker(null);
@@ -26,6 +28,17 @@ public class ForceMovementGods extends God
                 } else return -3;//cell is too high
             } else return-2;// throw new RuntimeException("Target cell is too far");
         } else return -1;//throw new RuntimeException("Target cell out of board");
+        return 1;
+    }
+
+    protected int MoveEnemy (Worker worker, Board board)
+    {
+        if (board.getCell(targetPosX, targetPosY).getWorker() == null || !board.getCell(targetPosX, targetPosY).getDome())
+        {
+            worker.setLastMovement(0);
+            board.getCell(targetPosX, targetPosY).setWorker(worker);
+        }
+        else return -5;//no space to move enemy worker
         return 1;
     }
 }
