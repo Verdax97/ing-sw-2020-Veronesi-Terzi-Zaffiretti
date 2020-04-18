ef059007689c7aef4726d8cacba7ac005f773c4f
Verdax97 <Verdax97@users.noreply.github.com>
2020-04-18 16:36:21 +0200

    Modified God.PlayerTurn and implemented Charon Prometheus

--- src/main/java/it/polimi/ingsw/model/God.java
+++ src/main/java/it/polimi/ingsw/model/God.java
@@ -10,7 +10,7 @@ public class God {
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
 
-    public void PlayerTurn(Board board, Player player) { }
+    public int PlayerTurn(Board board, Player player, int x, int y) { return 0; }
 
     public int Move(Board board, Cell selectedCell, int x, int y) { return 0; }
 
@@ -21,6 +21,24 @@ public class God {
     public void EnemyMove() { }
 
     public void EnemyBuilding() { }
+
+    protected Cell SearchAnyPlayerWorker(Board board, Player player, int x, int y)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                if (board.getCell(i, j).isAdjacent(x,y) && board.getCell(i, j).getWorker() != null)
+                {
+                    if (board.getCell(i, j).getWorker().getPlayer().getNickname().equals(player.getNickname()))
+                    {
+                        return board.getCell(i,j);
+                    }
+                }
+            }
+        }
+        return null;
+    }
 
     public Player WinCondition(Board board, Player player) { return null; }
 }
