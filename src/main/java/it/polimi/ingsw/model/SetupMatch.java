df842279bae4ff32362146dd83b3fd0c0ee429b1
euge9 <eugenio.terzi@mail.polimi.it>
2020-05-03 16:47:07 +0200

    Full coverage per SetupMatchTest

--- src/main/java/it/polimi/ingsw/model/SetupMatch.java
+++ src/main/java/it/polimi/ingsw/model/SetupMatch.java
@@ -1,12 +1,32 @@
 package it.polimi.ingsw.model;
 
+import it.polimi.ingsw.model.gods.*;
+
 import java.util.ArrayList;
 
 public class SetupMatch {
 
     private ArrayList<God> godList;
-    private ArrayList<God> godPicked;
+    private ArrayList<God> godPicked = new ArrayList<God>();
     private ArrayList<Player> players = new ArrayList<Player>();
+
+    public SetupMatch() {
+        godList = new ArrayList<God>();
+        this.AddGod(new Apollo());
+        this.AddGod(new Artemis());
+        this.AddGod(new Athena());
+        this.AddGod(new Atlas());
+        this.AddGod(new Charon());
+        this.AddGod(new Chrono());
+        this.AddGod(new Demeter());
+        this.AddGod(new Hephaestus());
+        this.AddGod(new Hestia());
+        this.AddGod(new Minotaur());
+        this.AddGod(new Pan());
+        this.AddGod(new Prometheus());
+        this.AddGod(new Triton());
+        this.AddGod(new Zeus());
+    }
 
 
     public God PickGod(int i)
@@ -45,6 +65,7 @@ public class SetupMatch {
     {
         this.players = players;
     }
+
     public void setPlayers(ArrayList<String> player)
     {
         for (String elem: player)
@@ -52,4 +73,5 @@ public class SetupMatch {
             players.add(new Player(elem));
         }
     }
+
 }
--- src/test/java/it/polimi/ingsw/model/SetupMatchTest.java
+++ src/test/java/it/polimi/ingsw/model/SetupMatchTest.java
@@ -18,4 +18,15 @@ public class SetupMatchTest
         assertEquals("player are wrong", players.get(0), setup.getPlayers().get(0).getNickname());
         assertEquals("player are wrong", players.get(1), setup.getPlayers().get(1).getNickname());
     }
+
+    @Test
+    public void getGodPicked(){
+        // First Look, NOT FINISHED
+        ArrayList<Player> players = new ArrayList<Player>();
+        SetupMatch setupMatch = new SetupMatch();
+        setupMatch.SetPlayers(players);
+        setupMatch.AddGodPicked(setupMatch.getGodList().get(0));
+        assertEquals("SetupMatch Error", "Apollo", setupMatch.getGodPicked().get(0).name);
+        assertEquals("SetupMatch Error", "Apollo", setupMatch.PickGod(0).name);
+    }
 }
