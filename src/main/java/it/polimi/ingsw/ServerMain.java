3995f46c5bd8659c94ff58f7d6e42285c3c3b552
Verdax97 <Verdax97@users.noreply.github.com>
2020-05-21 11:42:19 +0200

    added javadoc

--- src/main/java/it/polimi/ingsw/ServerMain.java
+++ src/main/java/it/polimi/ingsw/ServerMain.java
@@ -2,11 +2,16 @@ package it.polimi.ingsw;
 
 import it.polimi.ingsw.controller.Controller;
 import it.polimi.ingsw.view.server.ServerMultiplexer;
-
 /**
- * Hello world!
+ * Class ServerMain starts the server
+ *
+ * @author Davide
+ * Created on 21/05/2020
  */
 public class ServerMain implements Runnable {
+    /**
+     * Method run
+     */
     @Override
     public void run() {
         Controller controller = new Controller();
