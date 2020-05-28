1e501b3101eb11208b90de8fa702d99c5a574ce4
Verdax97 <Verdax97@users.noreply.github.com>
2020-05-28 16:13:00 +0200

    added javadoc

--- src/main/java/it/polimi/ingsw/view/client/ClientInputGUI.java
+++ src/main/java/it/polimi/ingsw/view/client/ClientInputGUI.java
@@ -10,36 +10,75 @@ import javafx.application.Platform;
 
 import java.io.IOException;
 
+/**
+ * Class ClientInputGUI receives a message and modify the gui
+ */
 public class ClientInputGUI extends ClientInput {
 
     private ControllerGUI controllerGui = null;
     private LobbyController lobbyController = null;
     private PickGodsController pickGodsController = null;
 
-    public SantoriniMatchController getSantoriniMatchController() {
-        return santoriniMatchController;
-    }
-
+    /**
+     * Method setSantoriniMatchController sets the santoriniMatchController of this ClientInputGUI object.
+     *
+     *
+     *
+     * @param santoriniMatchController the santoriniMatchController of this ClientInputGUI object.
+     *
+     */
     public void setSantoriniMatchController(SantoriniMatchController santoriniMatchController) {
         this.santoriniMatchController = santoriniMatchController;
     }
 
     private SantoriniMatchController santoriniMatchController = null;
 
-    public ControllerGUI getControllerGui() { return controllerGui; }
+    /**
+     * Method setControllerGui sets the controllerGui of this ClientInputGUI object.
+     *
+     *
+     *
+     * @param controllerGui the controllerGui of this ClientInputGUI object.
+     *
+     */
+    public void setControllerGui(ControllerGUI controllerGui) {
+        this.controllerGui = controllerGui;
+    }
 
-    public void setControllerGui(ControllerGUI controllerGui) { this.controllerGui = controllerGui; }
+    /**
+     * Method setLobbyController sets the lobbyController of this ClientInputGUI object.
+     *
+     *
+     *
+     * @param lobbyController the lobbyController of this ClientInputGUI object.
+     *
+     */
+    public void setLobbyController(LobbyController lobbyController) {
+        this.lobbyController = lobbyController;
+    }
 
-    public LobbyController getLobbyController() { return lobbyController; }
+    /**
+     * Method setPickGodsController sets the pickGodsController of this ClientInputGUI object.
+     *
+     *
+     *
+     * @param pickGodsController the pickGodsController of this ClientInputGUI object.
+     *
+     */
+    public void setPickGodsController(PickGodsController pickGodsController) {
+        this.pickGodsController = pickGodsController;
+    }
 
-    public void setLobbyController(LobbyController lobbyController) { this.lobbyController = lobbyController; }
+    /**
+     * Constructor ClientInput creates a new ClientInput instance.
+     *
+     * @param clientMain of type ClientMain
+     */
+    public ClientInputGUI(ClientMain clientMain) {
+        super(clientMain);
+    }
 
-    public PickGodsController getPickGodsController() { return pickGodsController; }
-
-    public void setPickGodsController(PickGodsController pickGodsController) { this.pickGodsController = pickGodsController; }
-
-    public ClientInputGUI(ClientMain clientMain) { super(clientMain); }
-
+    /** @see ClientInput#ParseMsg(MsgPacket) */
     @Override
     public void ParseMsg(MsgPacket msgPacket) {
 
@@ -48,14 +87,12 @@ public class ClientInputGUI extends ClientInput {
         if (msg.split(" ")[0].equalsIgnoreCase(Messages.error)) {
             String errorMsg = msg.split("\n", 2)[0];
             msg = msg.split("\n", 2)[1];
-            Platform.runLater(()-> {
-                controllerGui.error("Error", errorMsg);
-            });
+            Platform.runLater(() -> controllerGui.error("Error", errorMsg));
         }
 
         if (msg.equalsIgnoreCase(Messages.lobby)) {
             Platform.runLater(()-> {
-            try {
+                try {
                     controllerGui.changeToLobby(true);
                 } catch (IOException e) {
                     e.printStackTrace();
@@ -89,9 +126,7 @@ public class ClientInputGUI extends ClientInput {
         }
 
         if (msg.equalsIgnoreCase(Messages.choseGods) || msg.equalsIgnoreCase(Messages.choseYourGod)) {
-            Platform.runLater(() -> {
-                controllerGui.showGods(msgPacket.altMsg, true);
-            });
+            Platform.runLater(() -> controllerGui.showGods(msgPacket.altMsg, true));
         }
 
         if (msg.equalsIgnoreCase(Messages.waitTurn)){
@@ -151,6 +186,7 @@ public class ClientInputGUI extends ClientInput {
         }
     }
 
+    /** @see ClientInput#updateNotYourTurn(MsgPacket) */
     @Override
     public void updateNotYourTurn(MsgPacket msgPacket) {
         String msg = msgPacket.msg;
@@ -170,9 +206,7 @@ public class ClientInputGUI extends ClientInput {
         }
 
         if (msg.equalsIgnoreCase(Messages.choseGods) || msg.equalsIgnoreCase(Messages.choseYourGod)) {
-            Platform.runLater(() -> {
-                controllerGui.showGods(msgPacket.altMsg, false);
-            });
+            Platform.runLater(() -> controllerGui.showGods(msgPacket.altMsg, false));
         }
 
         if (msg.equalsIgnoreCase(Messages.placeWorkers)) {
